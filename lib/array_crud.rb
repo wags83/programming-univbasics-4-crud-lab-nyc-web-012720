c97d663697c225c6f372f1467e8c799f93e9c803
Greg Wagner <gpwagner@gmail.com>
2020-01-11 03:42:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,25 +15,25 @@ array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-
+array.pop
 end
 
 def remove_element_from_start_of_array(array)
-
+array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-
+array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-
+array[0]
 end
 
 def retrieve_last_element_from_array(array)
-
+array[array.length]
 end
 
 def update_element_from_index(array, index_number, element)
-
+array[index_number] = element
 end
